52bf2300a20b92da9567dfd6bf2e7a9d63f20f79
DymashevskijSP <sp.dymashevskij@gmail.com>
2023-03-01 23:34:20 +0300

    fixed linking

--- src/main/java/CommandParser.java
+++ src/main/java/CommandParser.java
@@ -1,21 +1,80 @@
-import Commands.Command;
-import Commands.Echo;
+import Commands.*;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 public class CommandParser {
-    public List<Command> parseCommands(String line){
-        List<Command> commands = new ArrayList<>();
-        String[] tokens = line.split(" ");
-        if(tokens[0].equals("echo")){
-            Command echo = new Echo();
-            for (int i = 1; i < tokens.length; i++) {
-                echo.addArgument(tokens[i]);
-            }
-            commands.add(echo);
-        }
-        return commands;
-    }
+    private Map<String, Command> commandMap = Map.ofEntries(
+            Map.entry("echo", new Echo()),
+            Map.entry("cat", new Cat()),
+            Map.entry("wc", new Wc()),
+            Map.entry("pwd", new Pwd()),
+            Map.entry("exit", new Exit())
+    );
 
+    public ArrayList<Command> parseCommands(String input) {
+        int i = 0;
+        ArrayList<Command> result = new ArrayList<>();
+        StringBuilder currentWord = new StringBuilder();
+        String curCommand = "";
+        ArrayList<String> curArgs = new ArrayList<>();
+        String curWord = "";
+        boolean inQuotes = false;
+        boolean inDoubleQuotes = false;
+        while (i < input.length()) {
+            char curChar = input.charAt(i);
+            if (inQuotes) {
+                if (curChar == '\'') {
+                    curArgs.add(currentWord.toString());
+                    currentWord = new StringBuilder();
+                } else {
+                    currentWord.append(curChar);
+                }
+            } else if (inDoubleQuotes) {
+                if (curChar == '\"') {
+                    curArgs.add(currentWord.toString());
+                    currentWord = new StringBuilder();
+                } else {
+                    currentWord.append(curChar);
+                }
+            } else {
+                if (curChar != ' ') {
+                    if (curChar == '\'') {
+                        inQuotes = true;
+                    } else if (curChar == '\"') {
+                        inDoubleQuotes = true;
+                    } else if (curChar == '|') {
+                        Command c = commandMap.get(curCommand);
+                        if (c != null) {
+                            for (String s : curArgs) {
+                                c.addArgument(s);
+                            }
+                            result.add(c);
+                            curCommand = "";
+                            curArgs = new ArrayList<>();
+                            currentWord = new StringBuilder();
+                        } else {
+                            System.exit(228);
+                            //вы проиграли
+                        }
+                    } else {
+                        currentWord.append(curChar);
+                    }
+                } else {
+                    String word = currentWord.toString();
+                    if (!word.equals("")) {
+                        if (curCommand.equals("")) {
+                            curCommand = word;
+                        } else {
+                            curArgs.add(word);
+                        }
+                    }
+                    currentWord = new StringBuilder();
+                }
+            }
+            i++;
+        }
+        System.out.printf("i found results %s", result.size());
+        return result;
+    }
 }
