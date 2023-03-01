dab5b0fd9ebfca66a40f131ee9376a084b534017
DymashevskijSP <sp.dymashevskij@gmail.com>
2023-03-01 19:46:39 +0300

    added some implementations

--- src/main/java/CommandParser.java
+++ src/main/java/CommandParser.java
@@ -1,11 +1,21 @@
 import Commands.Command;
+import Commands.Echo;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommandParser {
     public List<Command> parseCommands(String line){
-        return new ArrayList<>();
+        List<Command> commands = new ArrayList<>();
+        String[] tokens = line.split(" ");
+        if(tokens[0].equals("echo")){
+            Command echo = new Echo();
+            for (int i = 1; i < tokens.length; i++) {
+                echo.addArgument(tokens[i]);
+            }
+            commands.add(echo);
+        }
+        return commands;
     }
 
 }
--- src/main/java/CommandRunner.java
+++ src/main/java/CommandRunner.java
@@ -1,14 +1,29 @@
 import Commands.Command;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.util.List;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class CommandRunner {
-    public void runCommands(List<Command> commands){}
-    public void linkCommands(Command from, Command to){
-
+    public void runCommands(List<Command> commands) throws IOException {
+        commands.get(0).setInput(new PipedInputStream());
+        commands.get(0).setOutput(new PipedOutputStream());
+        for(int i = 1; i < commands.size(); i++){
+            linkCommands(commands.get(i - 1), commands.get(i));
+            commands.get(i).setOutput(new PipedOutputStream());
+        }
+        for(Command command: commands){
+            commandThreads.execute(command::processCommand);
+        }
+    }
+    public void linkCommands(Command from, Command to) throws IOException {
+        to.setInput(new PipedInputStream(from.getOutput()));
     }
     public void processException(Exception e){
 
     }
-    private List<Runnable> commandThreads;
+    private ThreadPoolExecutor commandThreads;
 }
